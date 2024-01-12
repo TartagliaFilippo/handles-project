7347a8dd4f4dead9f8038a83195296af5c3cf6cf
filippo199600 <pippotartaglia@gmail.com>
2024-01-12 17:31:03 +0100

    edit slider and add new images

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -3,37 +3,60 @@ export default {
   data() {
     return {
       title: "Products",
-      pdfList: [
+      activeSlide: 0,
+      imageList: [
         {
           name: "Antique Iron",
-          url: "../../public/pdf/Antique_iron.pdf",
+          url: "../../public/image-cataloghi/antique-iron.jpg",
         },
         {
           name: "Arredo Giardino",
-          url: "../../public/pdf/",
+          url: "../../public/image-cataloghi/arredo-giardino.jpg",
         },
         {
           name: "Arte e Design",
-          url: "../../public/pdf/",
+          url: "../../public/image-cataloghi/arte-design.jpg",
         },
         {
           name: "Catalogo Generale",
-          url: "../../public/pdf/",
+          url: "../../public/image-cataloghi/catalogo-generale.jpg",
         },
         {
           name: "Cucine e Dintorni",
-          url: "../../public/pdf/",
+          url: "../../public/image-cataloghi/catalogo-cucine.jpg",
         },
         {
           name: "Design e Colore",
-          url: "../../public/pdf/",
+          url: "../../public/image-cataloghi/design-colore.jpg",
         },
         {
           name: "l'Officina dello Stile",
-          url: "../../public/pdf/",
+          url: "../../public/image-cataloghi/officina-dello-stile.jpg",
         },
       ],
     };
+  },
+
+  methods: {
+    getSlide(index) {
+      this.activeSlide = index;
+    },
+
+    goPrev() {
+      if (this.activeSlide === 0) {
+        this.activeSlide = this.imageList.length - 1;
+      } else {
+        this.activeSlide--;
+      }
+    },
+
+    goNext() {
+      if (this.activeSlide === this.imageList.length - 1) {
+        this.activeSlide = 0;
+      } else {
+        this.activeSlide++;
+      }
+    },
   },
 };
 </script>
@@ -43,19 +66,26 @@ export default {
     <div class="container">
       <h1 class="main-title">{{ title }}</h1>
       <div class="carousel-container">
-        <div class="left-button">
+        <div class="left-button" @click="goPrev">
           <font-awesome-icon :icon="['fas', 'angle-left']" />
         </div>
-        <div class="left-preview">
-          <img src="../../public/image-cataloghi/antique-iron.jpg" alt="" />
+        <div
+          class="container-slider"
+          v-for="(image, index) in imageList"
+          :key="index"
+          :class="{ active: index === activeSlide }"
+        >
+          <div class="left-preview">
+            <img :src="image.url" alt="" />
+          </div>
+          <div class="main-image">
+            <img :src="image.url" alt="" />
+          </div>
+          <div class="right-preview">
+            <img :src="image.url" alt="" />
+          </div>
         </div>
-        <div class="image-container">
-          <img src="../../public/image-cataloghi/antique-iron.jpg" alt="" />
-        </div>
-        <div class="right-preview">
-          <img src="../../public/image-cataloghi/antique-iron.jpg" alt="" />
-        </div>
-        <div class="right-button">
+        <div class="right-button" @click="goNext">
           <font-awesome-icon :icon="['fas', 'angle-right']" />
         </div>
       </div>
@@ -70,44 +100,72 @@ export default {
   .container {
     width: 70%;
     margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: relative;
 
     .main-title {
       padding: 1rem 0 5rem;
+      text-align: center;
     }
-
     .carousel-container {
-      width: 90%;
-      height: 400px;
-      padding: 2rem 0;
+      width: 100%;
+      height: 70vh;
       background-color: #584935;
       position: relative;
-      border-radius: 20px;
+      border-radius: 1rem;
       display: flex;
       flex-direction: row;
-      justify-content: space-evenly;
-
+      justify-content: space-around;
+      align-items: center;
       .left-button,
       .right-button {
+        height: 100%;
         display: flex;
         align-items: center;
+        position: absolute;
+        padding: 0 1rem;
+        background-color: #ffff;
+        opacity: 0.5;
       }
-
-      .image-container {
-        img {
-          max-height: 100%;
+      .left-button {
+        left: 0;
+        border-radius: 1rem 0 0 1rem;
+      }
+      .right-button {
+        right: 0;
+        border-radius: 0 1rem 1rem 0;
+      }
+      .container-slider {
+        height: 90%;
+        width: 100%;
+        display: none;
+        &.active {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
         }
-      }
-
-      .left-preview,
-      .right-preview {
-        scale: 0.8;
-        img {
-          max-height: 100%;
+        .left-preview,
+        .right-preview {
+          width: 33%;
+          height: 100%;
+          scale: 0.8;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          img {
+            max-width: 100%;
+            max-height: 100%;
+          }
+        }
+        .main-image {
+          height: 100%;
+          width: 33%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          img {
+            max-width: 100%;
+            max-height: 100%;
+          }
         }
       }
     }
