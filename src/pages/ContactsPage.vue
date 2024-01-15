f5874a29bdbca59db6794287576febef9b21aeb8
filippo199600 <pippotartaglia@gmail.com>
2024-01-15 15:23:10 +0100

    feat contacts web3forms

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,27 +1,79 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+const WEB3FORMS_ACCESS_KEY = "ef56bc67-f4ef-4fdc-b8b8-9041d9b01fd7";
 
 export default {
   data() {
     return {
-      title: "Mettiti in contatto con noi",
+      title: "Contattaci",
+      name: "",
+      email: "",
+      message: "",
     };
   },
 
-  // components: {
-  //   MyComponent,
-  // },
+  methods: {
+    async submitForm() {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          access_key: WEB3FORMS_ACCESS_KEY,
+          name: this.name,
+          email: this.email,
+          message: this.message,
+        }),
+      });
+      const result = await response.json();
+      if (result.success) {
+        const sendMessageElement = document.querySelector(".send-message");
+        if (sendMessageElement) {
+          sendMessageElement.style.display = "block";
+
+          // timer messaggio inviato
+          setTimeout(() => {
+            sendMessageElement.style.display = "none";
+          }, 10000);
+        }
+        console.log(result);
+        this.name = "";
+        this.email = "";
+        this.message = "";
+      }
+    },
+  },
 };
 </script>
 
 <template>
   <div class="wrapper">
     <div class="container">
-      <h1>{{ title }}</h1>
-      <form action="mailto:pippotartaglia@gmail.com" method="get">
-        <label for="email-text">Testo email</label>
-        <input type="text" name="email-text" id="email-text" />
-        <button>invia</button>
+      <form @submit.prevent="submitForm">
+        <h1 class="main-title">{{ title }}</h1>
+        <div class="send-message">Il messaggio è stato inviato</div>
+        <input
+          type="text"
+          name="name"
+          v-model="name"
+          placeholder="Nome"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          v-model="email"
+          placeholder="Email"
+          required
+        />
+        <textarea
+          name="message"
+          v-model="message"
+          rows="4"
+          placeholder="In cosa possiamo aiutarti?"
+        ></textarea>
+        <button type="submit">Invia</button>
       </form>
     </div>
   </div>
@@ -34,10 +86,37 @@ export default {
   .container {
     width: 70%;
     margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+
+    .main-title {
+      padding: 1rem 0;
+      text-align: center;
+      font-size: 3rem;
+    }
+
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 2rem;
+      padding: 2rem 0;
+      .send-message {
+        display: none;
+        font-size: 1rem;
+        color: white;
+      }
+
+      & > * {
+        width: 30vw;
+        font-size: 1.5rem;
+        border-radius: 0.5rem;
+        padding: 1rem;
+      }
+
+      button {
+        width: auto;
+        padding: 0.5rem 1rem;
+      }
+    }
   }
 }
 </style>
--- src/pages/LocationPage.vue
+++ src/pages/LocationPage.vue
@@ -1,27 +1,38 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
 export default {
   data() {
     return {
       title: "Location",
     };
   },
-
-  // components: {
-  //   MyComponent,
-  // },
 };
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>{{ title }}</h1>
+    <div class="container">
+      <h1 class="main-title">{{ title }}</h1>
+      <div class="location-container"></div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .wrapper {
   min-height: 100vh;
+  .container {
+    width: 70%;
+    margin: 0 auto;
+
+    .main-title {
+      padding: 1rem 0 5rem;
+      text-align: center;
+    }
+    .location-container {
+      width: 100%;
+      height: 70vh;
+      border: 1px solid white;
+    }
+  }
 }
 </style>
