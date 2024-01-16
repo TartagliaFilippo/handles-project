88b9a984f05771a21faf72cc51f0185ae92c20d0
filippo199600 <pippotartaglia@gmail.com>
2024-01-16 15:25:45 +0100

    feat download PDF

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -9,30 +9,37 @@ export default {
         {
           name: "Antique Iron",
           url: "../../public/image-cataloghi/antique-iron.jpg",
+          pdfUrl: "public/pdf/antique-iron.pdf",
         },
         {
           name: "Arredo Giardino",
           url: "../../public/image-cataloghi/arredo-giardino.jpg",
+          pdfUrl: "public/pdf/arredo-giardino.pdf",
         },
         {
           name: "Arte e Design",
           url: "../../public/image-cataloghi/arte-design.jpg",
+          pdfUrl: "public/pdf/arte-design.pdf",
         },
         {
           name: "Catalogo Generale",
           url: "../../public/image-cataloghi/catalogo-generale.jpg",
+          pdfUrl: "public/pdf/catalogo-generale.pdf",
         },
         {
           name: "Cucine e Dintorni",
           url: "../../public/image-cataloghi/catalogo-cucine.jpg",
+          pdfUrl: "public/pdf/catalogo-cucine.pdf",
         },
         {
           name: "Design e Colore",
           url: "../../public/image-cataloghi/design-colore.jpg",
+          pdfUrl: "public/pdf/design-colore.pdf",
         },
         {
           name: "l'Officina dello Stile",
           url: "../../public/image-cataloghi/officina-dello-stile.jpg",
+          pdfUrl: "public/pdf/officina-dello-stile.pdf",
         },
       ],
     };
@@ -96,6 +103,22 @@ export default {
       this.goNext();
       this.autoSlide = false;
     },
+
+    downloadPDF() {
+      const currentImage = this.imageList[this.activeSlide];
+      const pdfUrl = currentImage.pdfUrl;
+
+      const link = document.createElement("a");
+      link.href = pdfUrl;
+      link.target = "_blank";
+      link.download = `download-${currentImage.name}.pdf`;
+
+      document.body.appendChild(link);
+      link.click();
+
+      // Rimuovi l'elemento <a> dopo il download
+      document.body.removeChild(link);
+    },
   },
 
   computed: {
@@ -123,6 +146,10 @@ export default {
   <div class="wrapper">
     <div class="container">
       <h1 class="main-title">{{ title }}</h1>
+      <p>
+        Per scaricare il pdf dei cataloghi devi cliccare sull'immagine in primo
+        piano
+      </p>
       <div class="carousel-container">
         <div class="left-button" @click="handlePrevClick">
           <font-awesome-icon :icon="['fas', 'angle-left']" />
@@ -134,13 +161,13 @@ export default {
           :class="{ active: index === activeSlide }"
         >
           <div class="left-preview">
-            <img :src="imageList[prevIndex].url" alt="" />
+            <img :src="imageList[prevIndex].url" :alt="image.name" />
           </div>
-          <div class="main-image">
-            <img :src="image.url" alt="" />
+          <div class="main-image" @click="downloadPDF">
+            <img :src="image.url" :alt="image.name" />
           </div>
           <div class="right-preview">
-            <img :src="imageList[nextIndex].url" alt="" />
+            <img :src="imageList[nextIndex].url" :alt="image.name" />
           </div>
         </div>
         <div class="right-button" @click="handleNextClick">
@@ -156,6 +183,14 @@ export default {
           @click="selectThumb(index)"
         ></div>
       </div>
+      <div class="pdf-list">
+        <h3>Puoi visionare l'interno dei cataloghi da uno di questi link:</h3>
+        <ul>
+          <li v-for="pdf in imageList">
+            <a :href="pdf.pdfUrl">{{ pdf.name }}</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -169,8 +204,12 @@ export default {
     margin: 0 auto;
 
     .main-title {
-      padding: 1rem 0 3rem;
+      padding: 1rem 0 2rem;
       text-align: center;
+    }
+
+    p {
+      color: #c9c9c9;
     }
     .carousel-container {
       width: 100%;
@@ -182,6 +221,7 @@ export default {
       justify-content: space-around;
       align-items: center;
       border-radius: 1rem;
+      margin: 1rem 0;
       .left-button,
       .right-button {
         height: 100%;
@@ -189,6 +229,7 @@ export default {
         align-items: center;
         position: absolute;
         padding: 0 1rem;
+        // color: #c9c9c9;
         background-color: #ffff;
         opacity: 0.5;
         cursor: pointer;
@@ -253,6 +294,18 @@ export default {
         background-color: #ffff;
         &.active {
           opacity: 0.3;
+        }
+      }
+    }
+    .pdf-list {
+      text-align: center;
+      padding: 1rem 0;
+      li {
+        list-style: none;
+        line-height: 1.5rem;
+        a {
+          text-decoration: none;
+          color: #c9c9c9;
         }
       }
     }
